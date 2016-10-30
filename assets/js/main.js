c206fa9be43ecb4b61eec66621ac0301978b6a3d
xuechenli <xuechenli@foxmail.com>
2016-10-29 22:14:54 -0500

    Add Google Analytics

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,16 @@
 
 (function($) {
 
+	/** Google Analytics **/
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  	ga('create', 'UA-86483693-1', 'auto');
+  	ga('send', 'pageview');
+	/** End **/
+	
 	var current_title = $(document).attr('title');
 	window.onblur = function () { document.title = "Hire Me \u2665"; }
 	window.onfocus = function () { document.title = current_title; }
